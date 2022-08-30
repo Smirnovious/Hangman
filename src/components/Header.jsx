e40d2e7c278e15caa6c3e04de37fe84412f7aa2d
Smirnovious <alex0401@gmail.com>
2022-08-30 12:05:26 +0300

    Project started

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Hangman</h1>
+        <h2>Find The Hidden Word - Enter A Letter</h2>
+    </div>
+  )
+}
+
+export default Header
